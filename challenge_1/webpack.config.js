6b9bb703d1b0c50e595252907e82a03a5857a7ac
Alex Burkowsky <aburkowsky9@gmail.com>
2018-12-05 11:43:57 -0800

    add App and Search component files

--- challenge_1/webpack.config.js
+++ challenge_1/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     path.resolve(__dirname, 'client/src/index.jsx'),
   ],
   output: {
-    path: path.resolve(__dirname, 'public/dist'),
+    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
   },
   module: {
