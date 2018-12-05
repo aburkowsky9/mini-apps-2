add6d48895b26fe223cac06354c53b99128ca000
Alex Burkowsky <aburkowsky9@gmail.com>
2018-12-05 13:01:19 -0800

    fetch query filtered data from json server

--- challenge_1/client/components/App.jsx
+++ challenge_1/client/components/App.jsx
@@ -1,7 +1,57 @@
 import React from 'react';
+import Results from './Results.jsx';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      keyword: '',
+      searchResults: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
+  handleChange({ target: { name, value } }) {
+    const newState = {};
+    newState[name] = value;
+
+    this.setState(newState);
+  }
+
+  async handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const results = await fetch(`/events?q=${this.state.keyword}`);
+      if (results.ok) {
+        const data = await results.json();
+        this.setState({
+          keyword: '',
+          searchResults: data,
+        });
+      } else {
+        throw new Error(results);
+      }
+    } catch(err) {
+
+    }
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Histoogle!</h1>
+        <form onSubmit={ this.handleSubmit }>
+          <label>
+            <span>Enter Historical Keyword:</span>
+            <input name="keyword" type="text" value={ this.state.keyword } onChange={ this.handleChange } required/>
+          </label>
+          <button type="submit">Search</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
--- challenge_1/webpack.config.js
+++ challenge_1/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: [
     '@babel/polyfill', // to allow async/await
-    path.resolve(__dirname, 'client/src/index.jsx'),
+    path.resolve(__dirname, 'client/index.jsx'),
   ],
   output: {
     path: path.resolve(__dirname, 'public'),
